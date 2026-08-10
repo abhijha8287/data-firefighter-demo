ead1b63bd01caa906bb2b46ad1419c9c6b85da15
Abhishek Jha <130655478+abhijha8287@users.noreply.github.com>
2026-08-10 17:14:36 +0530

    fix: remediate customer_email incident

--- customer_segmentation.sql
+++ customer_segmentation.sql
@@ -1,13 +1,4 @@
--- dataset: customer_segmentation
--- depends_on: analytics.customers
--- urn: urn:li:dataset:(urn:li:dataPlatform:snowflake,customer_segmentation,PROD)
---
--- BROKEN: segments customers by email domain (e.g. corporate vs. free
--- webmail), which requires analytics.customers.customer_email.
-
 SELECT
-    customer_id,
-    customer_email,
-    SPLIT_PART(customer_email, '@', 2) AS email_domain,
-    country_code
+  customer_id,
+  country_code
 FROM analytics.customers;
